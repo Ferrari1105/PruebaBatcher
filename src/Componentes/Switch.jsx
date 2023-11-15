5974b0b934905550acacab06b92daa2b9786701a
Rocco Ferrari <roccoferrari5@gmail.com>
2023-11-15 09:59:07 -0300

    aaa

--- src/Componentes/Switch.jsx
+++ src/Componentes/Switch.jsx
@@ -5,21 +5,25 @@ import { useContext } from "react"
 import { ThemeContext } from "../Context/ThemeContext"
 function SwitchExample() {
     const [isSwitchOn, setIsSwitchOn] = useState(false);
-    const [ThemeG, setThemeG] = useState(false);
+    const {ThemeG, setThemeG} = useContext(ThemeContext)
 
-  const handleSwitchChange = () => {
+    const handleSwitchChange = () => {
+        console.log("11111", ThemeG)
+        console.log("222222",localStorage.getItem("Theme"))
       setIsSwitchOn(!isSwitchOn);
-      localStorage.setItem("theme", isSwitchOn)
+      localStorage.setItem("Theme", !isSwitchOn)
+      setThemeG(!isSwitchOn)
   };
 
   return (
-    <div className={isSwitchOn ? 'switch-on' : 'switch-off'}>
+    <div className={ThemeG ? 'switch-on' : 'switch-off'}>
       <Form>
         <Form.Check
           type="switch"
           id="custom-switch"
-          label="Check this switch"
+          label="Cambiar el color"
           onChange={handleSwitchChange}
+          checked={ThemeG}
         />
       </Form>
     </div>
